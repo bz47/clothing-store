1d85868e31e8d731ea076a07c3ba17d34b202fe8
b-ez47 <noreply>
2023-07-25 10:10:03 -0400

    fixing responsivness

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import Nav from "./Components/Nav";
-import Burger from "./Components/Burger";
 import Banner from "./Components/Banner";
 
 function App() {
   return (
     <div className="App">
-      <Burger />
       <Nav />
       <Banner />
     </div>
