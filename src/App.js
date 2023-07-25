8032826fcd288f361374584f781eefdb7e51ee4b
b-ez47 <noreply>
2023-07-25 09:24:05 -0400

    add burger menu

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Nav from "./Components/Nav";
+import Burger from "./Components/Burger";
 import Banner from "./Components/Banner";
 
 function App() {
   return (
     <div className="App">
+      <Burger />
       <Nav />
       <Banner />
     </div>
---
+++ src/Components/Burger.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "../Styles/Burger.module.css";
+
+function Burger() {
+  return (
+    <div id={styles.burger} className="material-symbols-outlined">
+      menu
+    </div>
+  );
+}
+
+export default Burger;
