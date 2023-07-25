2b1c24014fcc4216342b2bf158bb3efb5570a06f
b-ez47 <noreply>
2023-07-25 10:37:28 -0400

    finish landing page responsivness

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -5,18 +5,18 @@ function Nav() {
   return (
     <div className={styles.nav}>
       <h2> Name</h2>
-      <div className={styles.button}>
+      <div id={styles.kids} className={styles.button}>
         <a href="">Kids</a>
       </div>
-      <div className={styles.button}>
+      <div id={styles.women} className={styles.button}>
         <a href="">Women</a>
       </div>
-      <div className={styles.button}>
+      <div id={styles.men} className={styles.button}>
         <a href="">Men</a>
       </div>
-      <input type="text" className={styles.search} placeholder="Search" />
+      <input type="text" id={styles.search} placeholder="Search" />
       <div className={styles.login}>
-        <button className={styles.login_img} href=""></button>
+        <button id={styles.login} className={styles.login_img} href=""></button>
       </div>
       <div className={styles.basket}>
         <button className={styles.basket_img}></button>
